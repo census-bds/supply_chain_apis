7aa0958b1d2a26cf7b19f430dfbced66fd420142
Krista Cheung Chan (CENSUS/ERD CTR) <krista.c.chan@census.gov>
2022-10-04 14:40:47 -0400

    fix intl trade url

--- data_source.py
+++ data_source.py
@@ -1,26 +1,31 @@
 import pandas as pd
 import requests
+import csv
 from config import CENSUS_API_KEY
 from urls import BASE_URL_CENSUS
 
 class DataSource():
-    def most_recent_naics(self, naics):
+    def most_recent_naics(self, naics, year):
         # Harmonizes naics to the most recent version
         return
 
+    def product_hs_code(self, code, code_type):
+        # Harmonizes product codes to the most recent HS
+        return
     pass
 
 class Api(DataSource):
     def __init__(self):
         self.url = BASE_URL_CENSUS
+        self.file_path = 'data/'
 
     def clean_api_text(self, api_text):
-        api_rows = api_text.split()
+        api_rows = api_text.split('\n')
         api_rows[0] = api_rows[0][2:-2]
         for i in range(1, len(api_rows) - 1):
             api_rows[i] = api_rows[i][1:-2]
         api_rows[-1] = api_rows[-2][1:-2]
-        api_rows = [x.split(',') for x in api_rows]
+        api_rows = [x.split('","') for x in api_rows]
         for i in range(len(api_rows)):
             api_rows[i] = [val.replace('"', '') for val in api_rows[i]]
         return api_rows
@@ -30,6 +35,14 @@ class Api(DataSource):
         assert resp, "Invalid response"
         return self.clean_api_text(resp)
 
+    def write_csv(self, output, file_name): 
+        with open(self.file_path + file_name, 'w') as f:
+            writer = csv.writer(f)
+            for row in output:
+                writer.writerow(row)
+        return
+
+
     # def pull_by_commodity(self):
     #     pass
 
@@ -37,11 +50,14 @@ class Api(DataSource):
 class IntlTrade(Api):
     def __init__(self):
         super().__init__()
+        self.file_path = 'data/Intl Trade/'
 
-    def naics_lookup(self, exports=True, year=2022):
-        url = self.url + 'timeseries/intltrade/{}/statenaics?get=STATE,NAICS,ALL_VAL_YR&YEAR=2021&key={}'.format(
-            'exports' if exports else 'imports', CENSUS_API_KEY
-        )
+    def state_lookup(self, exports=True, year=2022):
+        url = self.url + \
+            'timeseries/intltrade/{}/statehs?get=STATE,E_COMMODITY,E_COMMODITY_LDESC,{}_VAL_MO&YEAR={}&MONTH=12&COMM_LVL=HS6&key={}'.format(
+                'exports' if exports else 'imports', 'ALL' if exports else 'GEN', year, CENSUS_API_KEY
+            )
+        print(url)
         return self.get_request(url)
 
 class EconomicCensus(Api):
@@ -60,8 +76,8 @@ class EconomicCensus(Api):
 
 
     def naics_lookup(self):
-        url = self.url + "2017/ecnbasic?get=ESTAB,ESTAB_F&for=state:*&NAICS2017=*&key={}".format(
+        url = self.url + "2017/ecnbasic?get=FIRM,FIRM_F,ESTAB,ESTAB_F&for=state:*&NAICS2017=*&key={}".format(
             CENSUS_API_KEY
         )
-        
+        print(url)
         return self.remove_d_flag(self.get_request(url))
