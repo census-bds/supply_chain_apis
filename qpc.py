d847b3c3a3a730b89f2406aae3054e0199f28e18
dich0001 <yong.dich@census.gov>
2022-12-12 12:06:27 -0500

    initial qpc data full and emergency utils scraper

---
+++ qpc.py
@@ -0,0 +1,69 @@
+import pandas as pd
+
+from data_source import Api
+from config import CENSUS_API_KEY
+from bs4 import BeautifulSoup as bs
+import pandas as pd 
+import io
+import openpyxl
+import requests
+
+class QPC(Api):
+    def __init__(self):
+        super().__init__()
+        self.file_path = 'data/QPC/'
+
+    def generate_urls(self, years): 
+        urls = []
+        for year in years: 
+            url = f"https://www2.census.gov/programs-surveys/qpc/tables/{year}"
+            tables = requests.get(url)
+            soup = bs(tables.content)
+
+            href_soup = soup.find_all('a', href=True)
+            for a in href_soup: 
+                if ".xlsx" in a['href']: 
+                    extracted_url = url + a['href']
+                    urls.append((a['href'], extracted_url))
+        return urls
+    
+    def clean_qpc_file(self, urls): 
+        dfs = []
+        new_unnamed = "Industry Coverage LT 50p"
+        unnamed = {
+            "Unnamed: 2": new_unnamed, 
+            "Unnamed: 5": new_unnamed, 
+            "Unnamed: 7": new_unnamed
+        }
+        header_sheet_configs = {
+            "FULL Utilization Rates": (4, "A:L"), 
+            "EMERGENCY Utilization Rates": (4, "A:H")
+        }
+        # enumerate allows us to look at tuples 
+        for label, url in enumerate(urls): 
+            content = requests.get(url).content
+            xl = pd.ExcelFile(content, engine = "openpyxl")
+
+            for name in xl.sheet_names: 
+                if name in header_sheet_configs: 
+                    df = pd.read_excel(
+                        xl, 
+                        name, 
+                        header = header_sheet_configs[name][0], 
+                        usecols = header_sheet_configs[name][1], 
+                        true_values = ['c']
+                    ) 
+                    df = df.rename(columns = unnamed)
+                    df[new_unnamed] = df[new_unnamed].fillna(False)
+                    df = df.replace('c', True)
+                    
+                    dfs.append((label, df))
+                    self.download_qpc_file(label, df)
+        return dfs
+
+    def download_qpc_file(self, label, df): 
+        new_filename = label.replace(".xlsx", ".csv")
+        file_path = self.file_path + new_filename
+        return df.to_csv(new_filename)
+    
+
