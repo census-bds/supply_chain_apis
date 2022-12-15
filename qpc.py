63ce1cc76dde4bedb41eb82c5cac43bf649db3f7
dich0001 <yong.dich@census.gov>
2022-12-15 15:35:33 -0500

    qpc update

--- qpc.py
+++ qpc.py
@@ -7,16 +7,17 @@ import pandas as pd
 import io
 import openpyxl
 import requests
+import os 
 
 class QPC(Api):
     def __init__(self):
         super().__init__()
         self.file_path = 'data/QPC/'
 
-    def generate_urls(self, years): 
+    def generate_urls(self, years=[2022, 2021]): 
         urls = []
         for year in years: 
-            url = f"https://www2.census.gov/programs-surveys/qpc/tables/{year}"
+            url = f"https://www2.census.gov/programs-surveys/qpc/tables/{year}/"
             tables = requests.get(url)
             soup = bs(tables.content)
 
@@ -40,8 +41,9 @@ class QPC(Api):
             "EMERGENCY Utilization Rates": (4, "A:H")
         }
         # enumerate allows us to look at tuples 
-        for label, url in enumerate(urls): 
+        for (label, url) in urls: 
             content = requests.get(url).content
+            print("url: ", url)
             xl = pd.ExcelFile(content, engine = "openpyxl")
 
             for name in xl.sheet_names: 
@@ -58,12 +60,17 @@ class QPC(Api):
                     df = df.replace('c', True)
                     
                     dfs.append((label, df))
-                    self.download_qpc_file(label, df)
+                    self.download_qpc_file(name, label, df)
         return dfs
 
-    def download_qpc_file(self, label, df): 
-        new_filename = label.replace(".xlsx", ".csv")
+    def download_qpc_file(self, sheet, label, df): 
+        new_filename = sheet.replace(" ", "_") + "_" + label 
+        new_filename = new_filename.replace(".xlsx", ".csv")
+        if not os.path.exists(self.file_path): 
+            os.makedirs(self.file_path)
+        
         file_path = self.file_path + new_filename
-        return df.to_csv(new_filename)
+        
+        return df.to_csv(file_path)
     
 
