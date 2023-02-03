ccbf613fc174dfe42a8c63e921a94824bdbc28bb
chan0348 <chan0348@erd-web013-dev.compute.csp1.census.gov>
2023-02-03 16:53:21 -0500

    still need to finish clean_combine_dfs

--- intltrade.py
+++ intltrade.py
@@ -32,102 +32,45 @@ class IntlTrade(data_source.Api):
     
     def lookup(self, endpoint, params):
         df = super().lookup(endpoint, params)
-        return self.add_geo_state(df)
+        return self.add_geo(df)
 
-    def add_geo_state(self, df):
+    def add_geo(self, df):
         if not isinstance(self.state_geoid_xwalk, pd.DataFrame):
             self.state_geoid_xwalk = pd.read_csv(STATE_GEOID_CROSSWALK)
+        geo_col = None
         if 'STATE' in df.columns:
             df = df.merge(
                 self.state_geoid_xwalk[['GEO_ID', 'STATE']], on='STATE'
             )
-            df.loc[df['STATE'] == "-", "GEO_ID"] = "0100000US"
+            geo_col = 'STATE'
+        #Making a custom GEO_ID for ports that is "PORT" plus the port code
+        if 'PORT' in df.columns:
+            df['GEO_ID'] = df['PORT'].apply(lambda x: 'PORT' + str(x))
+            geo_col = 'PORT'
+        if geo_col:
+            df.loc[df[geo_col] == "-", "GEO_ID"] = "0100000US"
+            df.drop(columns=[geo_col], inplace=True)
+        else:
+            df['GEO_ID'] = "0100000US"
         return df
+    
+    def rename_hs_columns(self, dfs):
+        for df in dfs:
+            hs_cols = [col for col in df.columns if '_COMMODITY' in col]
+            for col in hs_cols:
+                df.rename(columns={col: 'HS'}, inplace=True)
+        return dfs
 
-
-    def geo_hs_lookup(self, geo=None, hs='HS6', exports=True, year=2021, datetype='year'):
-        '''
-        Grabs total value of shipments for the year.
-        Inputs:
-        geo: either 'state' or 'port' or None. None will grab nationwide values.
-        hs: The HS level to split on. Options are 'HS2', 'HS4', or 'HS6'
-        exports: True if exports, False if imports
-        year: the year to pull
-        '''
-        self.check_year(year, 1, 0)
-        url = self.url + \
-            'timeseries/intltrade/{}/{}hs?get={}{}_COMMODITY,{}_VAL_{}&YEAR={}&MONTH={}&COMM_LVL={}&key={}'.format(
-                'exports' if exports else 'imports',
-                geo if geo else '',
-                geo.upper() + "," if geo else "",
-                'E' if exports else 'I',
-                'ALL' if exports else 'GEN',
-                'YR' if datetype == 'year' else 'MO',
-                year,
-                '12' if datetype == 'year' else '*',
-                hs,
-                CENSUS_API_KEY
-            )
-        print(url)
-        return self.get_request(url)
-
-
-    def combine_geo(self, geo=None, hs='HS6', years=(2020, 2021), datetype='year'):
-        # Calls APIs and returns imports and exports joined and cleaned in a dataframe
-        all_years = []
-        for year in range(years[0], years[1] + 1):
-            imp = self.geo_hs_lookup(
-                geo=geo, hs=hs, exports=False, year=year, datetype=datetype
-            )
-            exp = self.geo_hs_lookup(
-                geo=geo, hs=hs, year=year, datetype=datetype
-            )
-            val_suffix = 'YR' if datetype == 'year' else 'MO'
-            drop_cols = ['YEAR', 'COMM_LVL', 'MONTH'] \
-                if datetype == 'year' else ['YEAR', 'COMM_LVL']
-            imp = pd.DataFrame(data=imp[1:], columns=imp[0]).drop(
-                columns=drop_cols
-            )
-            imp = imp.loc[imp['GEN_VAL_{}'.format(val_suffix)] != '0']
-            exp = pd.DataFrame(data=exp[1:], columns=exp[0]).drop(
-                columns=drop_cols
-            )
-            exp = exp.loc[exp['ALL_VAL_{}'.format(val_suffix)] != '0']
-            add_merge_cols = []
-            if geo:
-                add_merge_cols.append(geo.upper())
-            if datetype == 'month':
-                add_merge_cols.append('MONTH')
-            combined = imp.merge(
-                exp,
-                left_on=['I_COMMODITY'] + add_merge_cols,
-                right_on=['E_COMMODITY'] + add_merge_cols,
-                how='outer'
-            )
-            if geo:
-                combined = combined.loc[combined[geo.upper()] != "-"]
-            combined['YEAR'] = year
-            all_years.append(combined)
-        all_years_combined = pd.concat(all_years)
-        all_years_combined[hs] = all_years_combined.apply(
-            lambda x: x['I_COMMODITY'] if pd.notna(x['I_COMMODITY']) else x['E_COMMODITY'],
-            axis=1
-        )
-        all_years_combined.drop(
-            columns=['I_COMMODITY', 'E_COMMODITY'], inplace=True
-        )
-        all_years_combined.rename(
-            columns={
-                'GEN_VAL_{}'.format(val_suffix): 'import_value',
-                'ALL_VAL_{}'.format(val_suffix): 'export_value'
-            },
-            inplace=True
-        )
-        all_years_combined['import_value'] = all_years_combined['import_value'].apply(
-            lambda x: 0 if pd.isnull(x) else x
-        )
-        all_years_combined['export_value'] = all_years_combined['export_value'].apply(
-            lambda x: 0 if pd.isnull(x) else x
-        )
-        return all_years_combined
+    def clean_combine_dfs(self, dfs):
+        #Somewhat custom function for SCIP which will merge all dfs into one and rename columns
+        dfs = self.rename_hs_columns(dfs)
+        join_cols = ['HS', 'COMM_LVL', 'time', 'GEO_ID']
+        main_df = dfs[0]
+        for df in dfs[1:]:
+            join_on = [
+                col for col in join_cols \
+                    if col in main_df.columns and col in df.columns
+            ]
+            main_df = main_df.merge(df, on=join_on, how='outer')
+        return main_df
 
